c271c1a55fba4dba982f0c83187d9578e83a8e9a
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-28 15:56:51 -0800

    decreased batch size, increased batch number

--- seed.js
+++ seed.js
@@ -18,7 +18,7 @@ const writeToDatabase = (line, resolve, reject) => {
 
 const generateData = () => {
   const lines= [];
-  for (let i = 0; i < 200; i += 1) {
+  for (let i = 0; i < 50; i += 1) {
     const row = {};
     lineCounter += 1;
     const fakeName = `${faker.company.bsNoun()} ${faker.company.catchPhraseNoun()} ${faker.lorem.word()} ${lineCounter}`;
@@ -88,7 +88,7 @@ const seed = () => {
   });
   batchCounter += 1;
   Promise.all(promises).then(() => {
-    if (batchCounter < 100) {
+    if (batchCounter < 1000) {
       seed();
     } else {
       connection.shutdown();
