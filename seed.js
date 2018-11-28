1a1b71d1e29b617248c09558b5c04cc1ddc9c5dd
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-28 10:32:25 -0800

    promisified seeding process

--- seed.js
+++ seed.js
@@ -1,31 +1,42 @@
+/* eslint-disable no-console */
 const path = require('path');
-const stream = require('fs').createReadStream(path.join(__dirname, './data/restaurantsNoSQL.csv'));
-const readline = require('readline').createInterface({
-  input: stream,
-});
+const LineByLine = require('n-readlines');
+
+const csvFile = new LineByLine(path.join(__dirname, './data/restaurantsNoSQL.csv'));
 const connection = require('./connection');
 
 let lineCounter = 0;
+let promise;
 
-readline.on('line', (line) => {
-  if (line) {
-    readline.pause();
-    const query = 'INSERT INTO reservations.restaurants JSON ? ;';
-    connection.execute(query, [line], { prepare: true }, (error) => {
-      if (error) {
-        throw error;
-      } else {
-        lineCounter += 1;
-        if (lineCounter % 20000 === 0) {
-          console.clear();
-          console.log(`${((lineCounter / 10000000) * 100).toFixed(2)}% completed`);
-        }
-        if (lineCounter === 10000000) {
-          readline.close();
-          connection.shutdown();
-        }
-        readline.resume();
+const writeToDatabase = (line, resolve, reject) => {
+  const query = 'INSERT INTO reservations.restaurants JSON ? ;';
+  connection.execute(query, [line], { prepare: true }, (error) => {
+    if (error) {
+      reject(error);
+    } else {
+      lineCounter += 1;
+      if (lineCounter % 20000 === 0) {
+        console.clear();
+        console.log(`${((lineCounter / 10000000) * 100).toFixed(2)}% completed`);
       }
-    });
+      resolve();
+    }
+  });
+}
+
+const getPromise = () => new Promise((resolve, reject) => {
+  const line = csvFile.next().toString();
+  console.log(line);
+  if (line !== 'false') {
+    writeToDatabase(line, () => {
+      console.log(line);
+      promise.then(getPromise);
+      resolve();
+    }, reject);
+  } else {
+    connection.shutdown();
+    resolve();
   }
 });
+
+promise = getPromise();
