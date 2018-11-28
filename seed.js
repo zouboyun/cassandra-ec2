6790974d18b86c3be58708158785091652c78ec7
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-28 15:49:48 -0800

    lowered batch size

--- seed.js
+++ seed.js
@@ -18,7 +18,7 @@ const writeToDatabase = (line, resolve, reject) => {
 
 const generateData = () => {
   const lines= [];
-  for (let i = 0; i < 1000; i += 1) {
+  for (let i = 0; i < 100; i += 1) {
     const row = {};
     lineCounter += 1;
     const fakeName = `${faker.company.bsNoun()} ${faker.company.catchPhraseNoun()} ${faker.lorem.word()} ${lineCounter}`;
